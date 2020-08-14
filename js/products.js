9943cb9b4337b0a5b40479760d6be5d24bb41ea5
romii19 <romiiortin@gmail.com>
2020-08-14 16:52:14 -0300

    Agregar caaa

--- js/products.js
+++ js/products.js
@@ -17,7 +17,7 @@ function showCategoriesList(array){
                 </div>
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
-                        <h4 class="mb-1">`+ category.name +`</h4>
+                        <h4 class="mb-1">`+ category.name + category.currency + category.cost +`</h4>
                         <small class="text-muted">` + category.soldCount + ` artículos</small>
                         
                     </div>
