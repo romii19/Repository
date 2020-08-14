1c9288f86ef4f810cf4d4bef8b291cb7b2c84c88
romii19 <romiiortin@gmail.com>
2020-08-14 15:37:48 -0300

    Agregaar

--- js/products.js
+++ js/products.js
@@ -1,42 +1,14 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-const ORDER_ASC_BY_COST = "$->$$";
-const ORDER_DESC_BY_COST = "$$->$";
-const ORDER_DESC_BY_COST = "RR->R";
 var categoriesArray = [];
-var minCost = underFined;
-var maxCost = underFined;
-function sortProducts(criteria, array){
-    let result = [];
-    if (criteria === ORDER_ASC_BY_COST){
-        result = array.sort(function (a, b) {
-            if (a.cost < b.cost) { retur -1; }
-            if (a.cost > b.cost) { retur  1; }
-            return 0;
-        }
-    });
-} else if (criteria === ORDER_DESC_BY_COST) {
-    result = array.sort(function (a, b) {
-        result = array.sort(function (a, b) {
-            if (a.cost < b.cost) { retur -1; }
-            if (a.cost > b.cost) { retur  1; }
-            return 0;
-    }
-});
 
-return result;
-
-}
-
-function showProductsList(array)  {
+function showCategoriesList(array){
 
     let htmlContentToAppend = "";
     for(let i = 0; i < array.length; i++){
         let category = array[i];
-        
-        if(((minCost == undefined) || (minCost != undefined && parseInt(product.COST))))
-        if((maxCost == undefined) || (maxCost != undefined && parseInt(product.COST)))
+
         htmlContentToAppend += `
         <div class="list-group-item list-group-item-action">
             <div class="row">
@@ -46,7 +18,7 @@ function showProductsList(array)  {
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                         <h4 class="mb-1">`+ category.name +`</h4>
-                        <small class="text-muted">` + category.productCount + ` artículos</small>
+                        <small class="text-muted">` + category.soldCount + ` artículos</small>
                         
                     </div>
                  <small class="text-muted">` + category.description + ` artículos</small>
@@ -61,18 +33,18 @@ function showProductsList(array)  {
     }  
 }
 
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(LIST_URL).then(function(resultObj){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
         if (resultObj.status === "ok")
-        { 
-            productsArray = resultObj.data;
+        {
+            categoriesArray = resultObj.data;
 
-            prodectsArray = sortProducts(ORDER_ASC_BY_COST, productsArray);
+            showProductsList = showCategoriesList;
             //Muestro las categorías ordenadas
-            showProductsList(productsArray);
+            showCategoriesList(categoriesArray);
         }
     });
 });
-
-
-
