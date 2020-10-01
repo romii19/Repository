c0b149f8daffe3188f25950878eb3b43a9dafb4c
romii19 <romiiortin@gmail.com>
2020-10-01 20:34:39 -0300

    +++

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,24 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+var carrito = {};
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(CART_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok"){ 
+carrito = resultObj.data;
+var articles = '';
+for(i = 0; i < carrito.length; i++){
+   articles += carrito[i].name;
+}
+ document.getElementById("articles").innerHTML = articles;
+}
+}); 
+});   
 
-});
+
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function(e){});
